6e5cf031a6df0feadfe8b7eb73e852b37201a6c2
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-03-22 20:33:37 +0530

    ADD: welcome note to problem

--- LineCompare.java
+++ LineCompare.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+import java.lang.Math;
+
 public class LineCompare
 {
 	public static void main(String args[])
-	{
+	{	
 		System.out.println("This is line comparision problem");
+
+		Scanner a = new Scanner(System.in);
+		System.out.println("Enter x1 value: ");
+		int x1 = a.nextInt();
+
+		Scanner b = new Scanner(System.in);
+      System.out.println("Enter y1 value: ");
+      int y1 = b.nextInt();
+
+		Scanner c = new Scanner(System.in);
+      System.out.println("Enter x2 value: ");
+      int x2 = c.nextInt();
+
+		Scanner d = new Scanner(System.in);
+      System.out.println("Enter y2 value: ");
+      int y2 = d.nextInt();
+
+		double len = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
+		System.out.println(len); 		
+		
 	}
 }
